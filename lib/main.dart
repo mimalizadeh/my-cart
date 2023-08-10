8c35435357847d874bb37d2d08ea7cfb0b8e37e6
Mohammd <mo2215614@gmail.com>
2023-08-10 15:22:33 +0330

    add phone contaner and email contaner - add font and use matrial icons

--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[800],
         appBar: AppBar(
           title: const Text("My cart"),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: SafeArea(
-          // child: Container(
-          //   height: 100,
-          //   width: 100,
-          //   margin: const EdgeInsets.symmetric(vertical: 50, horizontal: 100),
-          //   padding: const EdgeInsets.fromLTRB(10, 20, 10, 30),
-          //   color: Colors.amberAccent,
-          //   child: const Text("Mohammad"),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
+          child: Column(
             children: [
-              Container(
-                color: Colors.orange[900],
-                child: const Text("Container 1"),
+              const CircleAvatar(
+                backgroundImage: AssetImage("images/avatar.jpeg"),
+                radius: 50,
+              ),
+              const Text(
+                "Mohammad Alizadeh",
+                style: TextStyle(
+                    fontFamily: "Pacifico",
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "Backend Developer",
+                style: TextStyle(
+                    fontFamily: "Dancing",
+                    color: Colors.amberAccent,
+                    letterSpacing: 2.5,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
               ),
               Container(
-                color: Colors.red[900],
-                height: 100,
-                width: 260,
-                child: const Text("Container 2"),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                padding: EdgeInsets.all(15),
+                child: const Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.blueGrey),
+                    SizedBox(width: 15),
+                    Text(
+                      "+98 915 123 22 33",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontFamily: "Pacifico",
+                          fontSize: 20,
+                          color: Colors.blueGrey),
+                    )
+                  ],
+                ),
               ),
               Container(
-                color: Colors.yellow[900],
-                child: const Text("Container 3"),
+                color: Colors.white,
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: const Row(
+                  children: [
+                    Icon(Icons.email,
+                    color: Colors.blueGrey,),
+                    SizedBox(width: 15),
+                    Text("mo2215614@gmail.com",
+                    style: TextStyle(
+                      fontFamily: "Pacifico",
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20,
+                      color: Colors.blueGrey
+                    ),)
+                  ],
+                ),
               )
             ],
           ),
